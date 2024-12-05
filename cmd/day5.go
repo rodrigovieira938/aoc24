2bb6abb48255eb0e05b60969a577820622b60db0
Rodrigo Vieira <rodizzvieira@gmail.com>
2024-12-05 12:33:27 +0000

    Day 5 - Puzzle 2

--- cmd/day5.go
+++ cmd/day5.go
@@ -58,7 +58,7 @@ func parse_data(str string) Data {
 }
 
 // Searchs in map or insert, if not found in map nor dependencies returns -1
-func search_in_map_or_insert(dependency int, dep_map map[int][]int, dependencies []Dependency) []int {
+func search_in_map_or_insert_second(dependency int, dep_map map[int][]int, dependencies []Dependency) []int {
 	val, ok := dep_map[dependency]
 	if ok {
 		return val
@@ -71,35 +71,82 @@ func search_in_map_or_insert(dependency int, dep_map map[int][]int, dependencies
 	return dep_map[dependency]
 }
 
-func puzzle1(data Data) int {
+func search_in_map_or_insert_first(dependency int, dep_map map[int][]int, dependencies []Dependency) []int {
+	val, ok := dep_map[dependency]
+	if ok {
+		return val
+	}
+	for _, dep := range dependencies {
+		if dep.first == dependency {
+			dep_map[dependency] = append(dep_map[dependency], dep.second)
+		}
+	}
+	return dep_map[dependency]
+}
+
+func is_update_correct(update Update, dependencies []Dependency, dep_map map[int][]int) bool {
+	ok := true
+
+	for i := len(update) - 1; i >= 0; i-- {
+		num := update[i]
+		dependencies := search_in_map_or_insert_second(num, dep_map, dependencies)
+
+		utils.SliceContains(dependencies, 0)
+
+		for _, dep := range dependencies {
+			index, found := utils.SliceContains(update, dep)
+			if found && index > i {
+				ok = false
+				break
+			}
+		}
+		if !ok {
+			break
+		}
+	}
+	return ok
+}
+
+func puzzle1(data Data) (int, []Update) {
+	sum := 0
+
+	dep_map := make(map[int][]int)
+	incorrectly_updates := []Update{}
+
+	for _, update := range data.Updates {
+
+		if is_update_correct(update, data.Dependencies, dep_map) {
+			sum += update[len(update)/2]
+		} else {
+			incorrectly_updates = append(incorrectly_updates, update)
+		}
+	}
+	return sum, incorrectly_updates
+}
+
+func puzzle2(data Data) int {
 	sum := 0
 
 	dep_map := make(map[int][]int)
 
 	for _, update := range data.Updates {
-		ok := true
-
-		for i := len(update) - 1; i >= 0; i-- {
-			num := update[i]
-			dependencies := search_in_map_or_insert(num, dep_map, data.Dependencies)
-
-			utils.SliceContains(dependencies, 0)
-
-			for _, dep := range dependencies {
-				index, found := utils.SliceContains(update, dep)
-				if found && index > i {
-					ok = false
-					break
+		for !is_update_correct(update, data.Dependencies, dep_map) {
+			for i := len(update) - 1; i >= 0; i-- {
+				num := update[i]
+				dependencies := search_in_map_or_insert_second(num, dep_map, data.Dependencies)
+				move := 0
+				for _, val := range dependencies {
+					index, found := utils.SliceContains(update, val)
+					if index > move && found {
+						move = index
+					}
 				}
-			}
-			if !ok {
-				break
+				n := update[i]
+				update[i] = update[move]
+				update[move] = n
 			}
 		}
-
-		if ok {
-			sum += update[len(update)/2]
-		}
+		sum += update[len(update)/2]
 	}
 	return sum
 }
@@ -108,6 +155,9 @@ func main() {
 	str, error := utils.ReadFileStr("./data/day5.txt")
 	if error == nil {
 		data := parse_data(str)
-		fmt.Printf("Day 5 - Puzzle 1 answer is %v\n", puzzle1(data))
+		sum, incorrectly_updates := puzzle1(data)
+		fmt.Printf("Day 5 - Puzzle 1 answer is %v\n", sum)
+		data.Updates = incorrectly_updates
+		fmt.Printf("Day 5 - Puzzle 2 answer is %v\n", puzzle2(data))
 	}
 }
